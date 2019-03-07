693fb97aee5ad2d832c70f97548541d00a5c6455
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-03-07 11:54:50 -0700

    Adds Model and impplements Cell

---
+++ AppleAlarm/WoldClock.swift
@@ -0,0 +1,14 @@
+//
+//  WoldClock.swift
+//  AppleAlarm
+//
+//  Created by Dominic Lanzillotta on 3/7/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import Foundation
+struct WorldClock {
+    let placeName: String
+    let time: Date
+    let timeChangeDescribtion: String
+}
--- AppleAlarm/WorldClockTableViewCell.swift
+++ AppleAlarm/WorldClockTableViewCell.swift
@@ -9,4 +9,23 @@
 import UIKit
 
 class WorldClockTableViewCell: UITableViewCell {
+    //MARK: - Outlets
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeChangeLabel: UILabel!
+    @IBOutlet weak var placeLabel: UILabel!
+    
+    //MARK: - Properties
+    var worldClock: WorldClock? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    //MARK: - Private Functions
+    func updateViews() {
+        guard let worldClock = worldClock else {return}
+        placeLabel.text = worldClock.placeName
+        timeLabel.text = "\(worldClock.time)"
+        timeChangeLabel.text = worldClock.timeChangeDescribtion
+    }
 }
